a96177692136179f00813c7fd39b3a530dd16b44
David <david.corbalan@vanadis.es>
2021-09-14 13:24:42 +0200

    Initial Commit

---
+++ exercise2films/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise2films
+//
+//  Created by David on 14/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
